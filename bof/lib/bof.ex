0d4575c62978510261cebb6f06462590b7ab7e1f
Jon Glick <jonathanpglick@gmail.com>
2019-01-25 15:08:21 -0800

    Makes the `Bof` api pipeable by making extra function heads to pull the shortcode out of the external game state map.

--- bof/lib/bof.ex
+++ bof/lib/bof.ex
@@ -10,35 +10,49 @@ defmodule Bof do
     |> Map.get(:shortcode)
   end
 
+  def add_team(%{shortcode: shortcode}, team_name), do: add_team(shortcode, team_name)
+
   def add_team(shortcode, team_name) do
     via_tuple(shortcode)
     |> GenServer.call({:add_team, team_name})
   end
+
+  def add_paper(%{shortcode: shortcode}, paper), do: add_paper(shortcode, paper)
 
   def add_paper(shortcode, paper) do
     via_tuple(shortcode)
     |> GenServer.call({:add_paper, paper})
   end
 
+  def can_start?(%{shortcode: shortcode}), do: can_start?(shortcode)
+
   def can_start?(shortcode) do
     via_tuple(shortcode)
     |> GenServer.call({:can_start?})
   end
+
+  def start(%{shortcode: shortcode}), do: start(shortcode)
 
   def start(shortcode) do
     via_tuple(shortcode)
     |> GenServer.call({:start})
   end
 
+  def turn_start(%{shortcode: shortcode}), do: turn_start(shortcode)
+
   def turn_start(shortcode) do
     via_tuple(shortcode)
     |> GenServer.call({:turn_start})
   end
 
+  def paper_guessed(%{shortcode: shortcode}), do: paper_guessed(shortcode)
+
   def paper_guessed(shortcode) do
     via_tuple(shortcode)
     |> GenServer.call({:paper_guessed})
   end
+
+  def state(%{shortcode: shortcode}), do: state(shortcode)
 
   def state(shortcode) do
     via_tuple(shortcode)
