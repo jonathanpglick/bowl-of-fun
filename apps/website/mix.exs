27362727022cf641a9799385c73b581712052305
Jon Glick <jonathanpglick@gmail.com>
2019-02-07 15:12:10 -0800

    Install Distillery.

--- apps/website/mix.exs
+++ apps/website/mix.exs
@@ -39,8 +39,7 @@ defmodule Website.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bof, in_umbrella: true},
-      {:distillery, "~> 2.0"}
+      {:bof, in_umbrella: true}
     ]
   end
 end
--- mix.exs
+++ mix.exs
@@ -15,6 +15,8 @@ defmodule BowlOfFun.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:distillery, "~> 2.0"}
+    ]
   end
 end
